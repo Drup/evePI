8ae2f8c62fdeb87f672eaa7bf194e48a4ff25747
Drup <drupyog@zoho.com>
2013-04-26 03:38:28 +0200

    Separate and comment Pi databases functions into proper modules

--- evePI_db.ml
+++ evePI_db.ml
@@ -1,9 +1,23 @@
+(** Various tables and queries related to the PI database *)
+(* Indentation with macaque is messy, propositions are welcomed. *)
+
+(**
+   Naming convention is the following:
+   - fetch for queries that return a list of concernd elements;
+   - by_ specify the criteria of selection;
+   - group_ specify how results are grouped. Grouped with {! list_grouping};
+   - get for queries that return properties on the object.
+
+*)
+
+
 open Eliom_lib
 open Eliom_content
 
+open Utility
 open Eve_db.PI
-					
-(* Tables pour la gestion des utilisateurs *)
+
+
 
 let users_id_seq = <:sequence< bigserial "users_id_seq" >>
 let users = 
@@ -61,69 +75,60 @@ let projects_admins =
 	user_id bigint NOT NULL
 	) >>
 
-(* Quelques fonctions utilitaires *)
-
 let q_currval seq = << { id = currval $seq$ } >>
 let get_currval seq = 
   lwt currval = view_one ~log:stderr (q_currval seq) in
   Lwt.return currval#!id
 
-let opt_map f = function
-	 None -> None 
-  | Some s -> Some (f s)
 
-let opt_string = function 
-	 Some s -> s
-  | None -> ""
 
-(** Projects *)
+(** {1 Projects} *)
 
-let exist_project project = 
+module Project = struct 
+let exist project = 
   (query
 	  <:select< row | 
 		row in $projects$ ;
 		row.id = $int64:project$ ;
 		>>)
   >|= (function [] -> false | _ -> true)
-
-(* Fetch des projets *)
-let get_all_projects () =
+  
+let fetch_all () =
   (query <:select< row | row in $projects$ >>)
   >|= List.map (fun r -> r#!id, r#!name, r#!description)
-					
-
-let get_my_projects id =
+	  
+let fetch_by_user id =
   (view
-		  << {id = project.id ; name = project.name } |
-			activity in $projects_users$ ;
-			project in $projects$ ;
-			project.id = activity.project_id ;
-			activity.user_id = $int64:id$ >>)
+   << {id = project.id ; name = project.name } |
+    activity in $projects_users$ ;
+    project in $projects$ ;
+    project.id = activity.project_id ;
+    activity.user_id = $int64:id$ >>)
   >|= List.map (fun p -> p#!id, p#!name)
 					
-let get_my_admin_projects id =
+let fetch_by_admin id =
   (view
-		  << {id = project.id ; name = project.name } |
-			activity in $projects_admins$ ;
-			project in $projects$ ;
-			project.id = activity.project_id ;
-			activity.user_id = $int64:id$ >>)
+   << {id = project.id ; name = project.name } |
+    activity in $projects_admins$ ;
+    project in $projects$ ;
+    project.id = activity.project_id ;
+    activity.user_id = $int64:id$ >>)
   >|= List.map (fun p -> p#!id, p#!name)
 
-
-(* Projets creation *)
-let new_project name description = 
+(** Create a new project, return the id of the project *)
+let create name description = 
   lwt _ = query ~log:stderr
-					 <:insert< $projects$ :=
-					  { id = projects?id ;
-					  name = $string:name$ ; 
-					  description = $string:description$ ;
-					  } >>
+	  <:insert< $projects$ :=
+	 		{ id = projects?id ;
+			name = $string:name$ ; 
+			description = $string:description$ ;
+			} >>
   in
   get_currval projects_id_seq
 	 
-(* Project information *)
-let get_project_details project_id =
+(** Various project informations *)
+
+let get_details project_id =
   (view_one
 				<< {name = project.name ; descr = project.description } |
 				 project in $projects$ ;
@@ -131,39 +136,44 @@ let get_project_details project_id =
 				 >>)
   >|= (fun r -> r#!name, r#!descr)
 
-let get_project_name project_id = 
+let get_name project_id = 
   get_project_details project_id >|= fst
-				  
-(** Project Tree *)
+				
+  
+(** Project tree related queries *)
 
-let fill_project_tree project tree = 
+(** Take an existing tree and add it to the {! projects_tree} table *)
+let fill_tree project tree = 
   let new_node parent typeid = 
-	 let parent = opt_map (fun p -> <:value< $int64:p$ >>) parent in
-	 lwt _ = query 
-						<:insert< $projects_tree$ := {
-						 project_id = $int64:project$ ;
-						 product_id = projects_tree?product_id ;
-						 parent_id = of_option $parent$ ;
-						 notes = null }				 
-						 >> in 
-	 lwt currval = get_currval projects_tree_product_id_seq in
-	 lwt _ = query 
-						<:insert< $products$ := {
-						 id = $int64:currval$ ;
-						 typeid = $int32:typeid$
-						 } >> in
-	 Lwt.return currval
+	let parent = opt_map (fun p -> <:value< $int64:p$ >>) parent in
+	lwt _ = query 
+		<:insert< $projects_tree$ := {
+				 project_id = $int64:project$ ;
+				 product_id = projects_tree?product_id ;
+				 parent_id = of_option $parent$ ;
+				 notes = null }				 
+				 >> in 
+	lwt currval = get_currval projects_tree_product_id_seq in
+	lwt _ = query 
+		<:insert< $products$ := {
+				 id = $int64:currval$ ;
+				 typeid = $int32:typeid$
+				 } >> in
+	Lwt.return currval
   in
   let rec traverse_tree parent tree = match tree with
-	 	Tree.Leaf typeid -> 
+	  Tree.Leaf typeid -> 
 		lwt _ = new_node parent typeid in Lwt.return ()
-	 | Tree.Node (typeid,tl) -> 
-		 lwt parent = new_node parent typeid in
-		 Lwt_list.iter_s (traverse_tree (Some parent)) tl
+	| Tree.Node (typeid,tl) -> 
+		lwt parent = new_node parent typeid in
+		Lwt_list.iter_s (traverse_tree (Some parent)) tl
   in
   traverse_tree None tree
 
-(* Fetch the project forest *)
+(** Two functions used to query the project tree recursivly. 
+	See also the {! Tree} module.
+*)
+
 let get_roots project = 
   (view 
 	  << { id = node.product_id; typeid = node_info.typeid ; note = node.notes} |
@@ -175,6 +185,7 @@ let get_roots project =
 		>>)
   >|= List.map (fun r -> r#!id, r#!typeid, opt_string r#?note)
 		  
+
 let get_sons pID = 
   (view
 	  << { id = node.product_id; typeid = node_info.typeid ; note = node.notes } |
@@ -185,26 +196,15 @@ let get_sons pID =
 		>>)
   >|= List.map (fun r -> r#!id,r#!typeid, opt_string r#?note)
 
-(* Modification d'un projet *)
-(*
-let change_projet_goal project_id new_goal new_tree = 
-  lwt _ = 
-	 query 
-    <:update< row in $projects$ := 
-	  { goal = $int32:new_goal$ } |
-	  row.project_id = $int64:project_id$
-	  >> in
-  lwt _ = 
-	 delete
-	 <:delete< row in $products$ |
-	  row.project_id = $int64:
->> 
-
-*)
+end
 	 
-(** Planets *)
 
-let new_planet ?prod ?note ?project user location =
+
+(** {1 Planets} *)
+
+module Planet = struct 
+
+let create ?prod ?note ?project user location =
   let project = opt_map (fun p -> <:value< $int64:p$ >>) project in
   let prod = opt_map (fun p -> <:value< $int64:p$ >>) prod in
   let note = opt_map (fun n -> <:value< $string:n$ >>) note in
@@ -218,34 +218,25 @@ let new_planet ?prod ?note ?project user location =
 	  location = $int32:location$ ;
 	  } >>
 
-let update_planet_project planet_id project =
+let update_project planet_id project =
   query
 	 <:update< p in $planets$ := { project_id = $int64:project$ } |
 	  p.id = $int64:planet_id$ 
 	  >>
 
-let update_planet_product planet_id product =
+let update_product planet_id product =
   query
 	 <:update< p in $planets$ := { product_id = $int64:product$ } |
 	  p.id = $int64:planet_id$ 
 	  >>
 
-let update_planet_notes planet_id notes =
+let update_notes planet_id notes =
   query
 	 <:update< p in $planets$ := { notes = $string:notes$ } |
 	  p.id = $int64:planet_id$ 
 	  >>
 
-(* Group an 'a * 'b list according to the 'a field. 
-THE LIST NEED TO BE ORDERED ! *)
-let list_grouping l = 
-  let aux l (a,b) = match l with 
-		[] -> [(a,[b])] 
-	 | (x,y)::t when x = a -> (a,b::y)::t
-	 | _ -> (a,[b])::l
-  in List.fold_left aux [] l
-
-let get_planets_by_user user =
+let fetch_by_user user =
   (view
 	  << { p = planet.id } |
 		planet in $planets$ ;
@@ -253,7 +244,7 @@ let get_planets_by_user user =
 		>>)
   >|= List.map (fun x -> x#!p)
 
-let get_planets_by_user_grouped_loc user =
+let fetch_by_user_group_loc user =
   (view
 	<< { proj = planet.project_id ;
 		  p = planet.id ; 
@@ -267,7 +258,7 @@ let get_planets_by_user_grouped_loc user =
   list_grouping (List.map 
 		(fun x -> x#!loc, (x#!p, x#?proj, x#?prod)) l))
 
-let get_planets_by_product product =
+let fetch_by_product product =
   (view
 	  << group { p = count[planet.id] ; u = u_ }
 		  by { u_ = user.name } |
@@ -278,7 +269,7 @@ let get_planets_by_product product =
 		>>)
   >|= List.map (fun x -> x#!u,Int64.to_int x#!p)
 
-let get_planets_by_project project =
+let fetch_by_project project =
   (view
 		  << { u = user.name  ; 
 			    p = planet.id ; 
@@ -292,18 +283,20 @@ let get_planets_by_project project =
   >|= (fun l -> 
 		 list_grouping (List.map (fun x -> x#!u, (x#!p, x#?prod)) l))
 
-let get_planets_by_project_user project user =
+let fetch_by_project_user project user =
   (view
-		  << { p = planet.id ; 
-		  loc = planet.location ;
+   << { p = planet.id ; 
+		loc = planet.location ;
         prod = planet.product_id } |
-			planet in $planets$ ;
-			planet.user_id = $int64:user$ ;
-			planet.project_id = $int64:project$
-			>>)
+    planet in $planets$ ;
+    planet.user_id = $int64:user$ ;
+    planet.project_id = $int64:project$
+	>>)
   >|= List.map (fun x -> x#!p,x#!loc,x#?prod)
 
-let get_free_user project_id = 
+
+(** Give the number of free planets for each user in a given project *)
+let fetch_free_by_user project_id = 
   (view
 		  << group { id = u_id ; name = u_name ; planets = count[planet.id] } by {u_id = planet.user_id ; u_name = user.name } |
 			planet in $planets$ ;
@@ -314,42 +307,40 @@ let get_free_user project_id =
 			>>)
   >|= List.map (fun x -> x#!id, x#!name, x#!planets)
 
-(** Users gestion *)
+end
 
-let exists_user name =
+
+
+(** {1 Users} *)
+module User = struct
+
+let exist name =
   (view
-		  <:view< {password = user.password} |
-         user in $users$;
-         user.name = $string:name$ >>)
+   << {password = user.password} |
+    user in $users$;
+    user.name = $string:name$ >>)
   >|= (function [] -> false | _ -> true)
 	 
-let get_user_id name = 
+let get_id name = 
   (view_one
-				<< {id = user.id } |
-				 user in $users$ ;
-				 user.name = $string:name$ 
-				 >>)
+   << {id = user.id } |
+    user in $users$ ;
+    user.name = $string:name$ 
+	>>)
   >|= (fun p -> p#!id)
 
 exception User_allready_exists
 
-let add_user name pwd =
+let create name pwd =
   lwt b = exists_user name in
   if not b then 
-	 query
-			 <:insert< $users$ :=
-			  { id = users?id ;
-			  name = $string:name$ ; 
-			  password = $string:pwd$  } >>
+	query
+   <:insert< $users$ :=
+			{ id = users?id ;
+			name = $string:name$ ; 
+			password = $string:pwd$  } >>
   else raise_lwt User_allready_exists
 
-let attach_admin project user =
-  query
-		  <:insert< $projects_admins$ :=
-			{ project_id = $int64:project$;
-			user_id = $int64:user$ ;
-			} >>
-		  
 (* TODO : make password secure *)
 let check_pwd name pwd =
   (view
@@ -359,7 +350,7 @@ let check_pwd name pwd =
 			user.password = $string:pwd$ >>)
   >|= (function [] -> false | _ -> true)
 
-let attach_user project user =
+let attach project user =
   query 
 		  <:insert< $projects_users$ := {
 			project_id = $int64:project$ ;
@@ -375,6 +366,15 @@ let is_attached project user =
 			 >>)
   >|= (function [] -> false | _ -> true)
 
+(** Admin stuff *)
+module Admin = struct 
+let promote project user =
+  query
+	<:insert< $projects_admins$ :=
+		  { project_id = $int64:project$;
+		  user_id = $int64:user$ ;
+		  } >>
+ 
 let is_admin project user = 
   (query
 			<:select< row |
@@ -383,4 +383,4 @@ let is_admin project user =
 			 row.project_id = $int64:project$ ;
 			 >>)
   >|= (function [] -> false | _ -> true)
-
+end
---
+++ utility.ml
@@ -0,0 +1,22 @@
+(* This file regroups all general and utility piece of code used in the project *)
+
+(** Opt related *)
+
+let opt_map f = function
+	 None -> None 
+  | Some s -> Some (f s)
+
+let opt_string = function 
+	 Some s -> s
+  | None -> ""
+
+(** List related *)
+
+(* Group an 'a * 'b list according to the 'a field. 
+THE LIST NEED TO BE ORDERED ! *)
+let list_grouping l = 
+  let aux l (a,b) = match l with 
+		[] -> [(a,[b])] 
+	 | (x,y)::t when x = a -> (a,b::y)::t
+	 | _ -> (a,[b])::l
+  in List.fold_left aux [] l
